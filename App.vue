5d6cee236947d4d45f389c558236749691cf2119
phares-gonzales <42355532+phares-gonzales@users.noreply.github.com>
2021-09-20 21:29:20 -0700

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <Home msg="Application for internal purposes only!" />
+    </div>
+</template>
+
+<script>
+    import Home from './components/Home.vue';
+    export default {
+        name: 'app',
+        components: {
+            Home
+        }
+    };
+</script>
+
+<style>
+</style>
